9295f5d0fadc9f1b53bf787a86c9612054046185
Liza Stadnichenko <6068011grell@gmail.com>
2024-10-03 15:13:08 +0300

    lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+char count = 'n';
+    while(count == 'n'){
+        int number1 = 0;
+        cout << "Введіть перше число: ";
+        cin >> number1;
+        int number2 = 0;
+        cout << "Введіть друге число: ";
+        cin >> number2;
+        int sum = number1 + number2;
+        cout<<"Сума чисел:"<<sum<<endl;
+        
+        cout<<"Хочете вийти? y/n:";
+        cin>>count;
+    }
+    
+    return 0;
+}
